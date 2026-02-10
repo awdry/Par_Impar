6c5732fb0d99ace79873c412ca3d27409b9bd541
Awdry Nolasco <nolascoawdry@gmail.com>
2026-02-10 00:00:29 -0400

    Programa que le solicite al usuario un numero e indique si es par o impar

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Usando los conocimientos adquiridos, usted debe desarrollar
+//un programa que le solicite al usuario un numero y el
+//programa indique si es par o impar.
+
+bool continueprogram = true;
+while (continueprogram)
+{
+   try{ 
+Console.WriteLine("Ingrese un número: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("El numero es par.");
+}
+else{
+    Console.WriteLine("El numero es impar.");
+}
+   }
+   catch (FormatException)
+    {
+        Console.WriteLine("ERROR DEBE DE INGRESAR UN NúMEOR VALIDO");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocurrió un error: {ex.Message}");
+    }
+
+    Console.ReadKey();
+
+}
